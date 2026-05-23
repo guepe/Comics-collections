e368e98d04ddbfed23f8e393da4939e109949fe9
Pierre-Henri Delaval <ph.delaval@gmail.com>
2026-05-23 11:04:44 +0200

    fix test case

--- comics_collections/tests/__init__.py
+++ comics_collections/tests/__init__.py
@@ -1,6 +1,3 @@
-from . import test_isbn
-from . import test_computed_fields
-from . import test_import_wizard
 from . import test_comic_isbn
 from . import test_comic_work
 from . import test_comic_edition
